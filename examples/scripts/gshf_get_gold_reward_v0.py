99b6241f823831275e317218c59b1b3d7355b914
Hanze Dong <18676401+hendrydong@users.noreply.github.com>
2023-12-25 21:51:30 +0800

    update gold reward computation

--- examples/scripts/gshf_get_gold_reward_v0.py
+++ examples/scripts/gshf_get_gold_reward_v0.py
@@ -1,74 +1,44 @@
-from transformers import pipeline, AutoTokenizer
-from transformers import GenerationConfig, AutoModelForCausalLM
-import time
-from torch.utils.data import DataLoader
-
+from datasets import load_dataset
+import torch
+from transformers import (
+    default_data_collator,
+    pipeline,
+    set_seed,
+    AutoTokenizer
+)
+import sys
 import os
 
-from dataclasses import dataclass, field
-from typing import Optional
+import numpy as np
+import matplotlib.pyplot as plt
+# 获取命令行参数列表
 from accelerate import Accelerator
-import json
-import torch
-from datasets import load_dataset, Dataset
-from tqdm import tqdm
-from transformers import AutoTokenizer, HfArgumentParser, pipeline, DataCollatorForSeq2Seq
+from accelerate.state import AcceleratorState
+import torch.distributed as dist
+
+accelerator = Accelerator()
+
+AcceleratorState().deepspeed_plugin.deepspeed_config['train_micro_batch_size_per_gpu'] = 4
+
+
+#####
+# This script takes a dataset as the input, where each sample is {"input": "the pormpt", "output": ["response1", "response2", "response3", ...]}
+# The script will compute the reward for each input-output pair, and eventually output a new dataset, where each sample contains {"input": "the pormpt", "output": ["response1", "response2", "response3", ...], "rewards": [reward1, reward2, ...]}
+# Due to memory constraint, we will set the reward of the input+output that is longer than 800 tokens as -999999, which should be discarded in later processing. It should be at most ~2% samples that are discarded.
+#####
+
+#Parameters
+#
+#
+#
+
+ds_dir = "/home/xiongwei/over_opt/LMFlow_RAFT_Dev/output_models/forgetting_proj/over_opt_raft3b_get_samples_by_if_model_max512/model0/infer_set/my_infer_set.json"
+output_dir = "/home/xiongwei/gshf_gen_data/LMFlow_RAFT_Dev/data/my_filtered_set.json"
+
 from transformers import PreTrainedModel, LlamaConfig, LlamaModel, LlamaTokenizer
 import torch.nn as nn
 import torch
 from typing import Optional, List
-import numpy as np
-import pandas as pd
-tqdm.pandas()
-from accelerate import Accelerator
-from accelerate.state import AcceleratorState
-
-from trl.trainer.utils import generate
-
-@dataclass
-class ScriptArguments:
-    """
-    The arguments for the DPO training script.
-    """
-    json_path: Optional[str] = field(
-        default="1000.json",
-        metadata={"help": "the location of the dataset name or path"},
-    )
-    output_dir: Optional[str] = field(
-        default="gen.json",
-        metadata={"help": "the location of the output file"},
-    )
-    batch_size: Optional[int] = field(
-        default=64,
-        metadata={"help": "the batch size for inference"},
-    )
-    max_length: Optional[int] = field(
-        default=9999999999,
-        metadata={"help": "the maximum length of the prompt"},
-    )
-    gold_reward_name_or_path: Optional[str] = field(
-        default="openbmb/UltraRM-13b",
-        metadata={"help": "the name of the gold reward model"},
-    )
-
-
-
-
-
-
-
-accelerator = Accelerator()
-
-#AcceleratorState().deepspeed_plugin.deepspeed_config['train_micro_batch_size_per_gpu'] = 4
-parser = HfArgumentParser(ScriptArguments)
-script_args = parser.parse_args_into_dataclasses()[0]
-
-
-
-ds_dir = script_args.json_path#"/home/xiongwei/over_opt/LMFlow_RAFT_Dev/output_models/forgetting_proj/over_opt_raft3b_get_samples_by_if_model_max512/model0/infer_set/my_infer_set.json"
-output_dir = script_args.output_dir#"/home/xiongwei/gshf_gen_data/LMFlow_RAFT_Dev/data/my_filtered_set.json"
-
-
 
 class LlamaRewardModel(PreTrainedModel):
     config_class = LlamaConfig
@@ -111,11 +81,11 @@ class LlamaRewardModel(PreTrainedModel):
 
 device = accelerator.device
 
-tokenizer = LlamaTokenizer.from_pretrained(script_args.gold_reward_name_or_path)
-model = LlamaRewardModel.from_pretrained(script_args.gold_reward_name_or_path).to(torch.bfloat16)
+tokenizer = LlamaTokenizer.from_pretrained("openbmb/UltraRM-13b")
+model = LlamaRewardModel.from_pretrained("openbmb/UltraRM-13b").to(torch.bfloat16)
 model = model.to(device)
 
-rm_tokenizer = AutoTokenizer.from_pretrained(script_args.gold_reward_name_or_path)
+rm_tokenizer = AutoTokenizer.from_pretrained("openbmb/UltraRM-13b")
 
 world_size = int(os.getenv("WORLD_SIZE", "1"))
 ####
@@ -129,14 +99,16 @@ share = int(data_size / world_size)
 ds = ds.select(np.arange(local_rank * share, (local_rank + 1)*share))
 responses_pos = [sample['input'] + sample['output'][0] for sample in ds]
 responses_neg =  [sample['input'] + sample['output'][1] for sample in ds]
-
-
+#N = 2000
 print(len(responses_pos), len(responses_neg))
 N = len(responses_neg)
 
+####
+
+optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=0.0001)
 
 model, optimizer = accelerator.prepare(
-    model
+    model, optimizer
 )
 
 
@@ -169,7 +141,7 @@ def change_of_format(txt):
 
 cnt = 0
 
-for sample in ds:
+for sample in dataset:
     test_texts = [change_of_format(sample['input'] + sample['output'][0]), change_of_format(sample['input'] + sample['output'][1])]
     rewards = get_reward(test_texts)
     data.append({"input": sample['input'], "output": sample['output'], "rewards": rewards})
@@ -188,9 +160,6 @@ all_process_list =[{}] * world_size
 data_to_send = {
     'data': [[data[i]] for i in range(len(data))]
 }
-
-import torch.distributed as dist
-
 dist.all_gather_object(all_process_list, data_to_send)
 gathered_data = []
 
