5592c1bf4eecf7da8abad876de10971c27169595
Hanze Dong <18676401+hendrydong@users.noreply.github.com>
2023-12-26 20:15:39 +0800

    Update gshf_get_rso_dataset.py

--- examples/scripts/gshf_get_rso_dataset.py
+++ examples/scripts/gshf_get_rso_dataset.py
@@ -54,6 +54,77 @@ class ScriptArguments:
         default="rso_gen.json",
         metadata={"help": "the location of the output file"},
     )
+    num_samples_per_prompt: Optional[int] = field(
+        default=2,
+        metadata={"help": "the number of samples to generate per prompt"},
+    )
+    ranking_type: Optional[str] = field(
+        default="first_round",
+        metadata={"help": "the ranking method"},
+    )
+    beta: Optional[float] = field(
+        default=0.5,
+        metadata={"help": "the beta value for rejection sampling"},
+    )
+    input_output_delimiter: Optional[str] = field(
+        default=" ",
+        metadata={"help": "the delimiter between input and output"},
+    )
+
+
+def first_round_ranking(responses: List[str], rewards: List[float]) -> Tuple[List[str], List[str]]:
+    """Conducts first round ranking. Starts from n responses and construct n/2 pairs to be assigned
+    to chosen or rejected based on there rewards.
+    
+    Args:
+        responses: accecpted candidates from rejection sampling
+        rewards: response rewards.
+        
+    Returns:
+        chosen: chosen samples.
+        rejected: rejected samples.
+    """
+    
+    chosen = []
+    rejected = []
+    
+    def pick(responses):
+        selected = random.randrange(len(responses))
+        return responses.pop(selected)
+    
+    responses = [(response, reward) for response, reward in zip(responses,rewards)]
+    while responses:
+        selected1 = pick(responses)
+        selected2 = pick(responses)
+        if selected1[1]>selected2[1]:
+            chosen.append(selected1[0])
+            rejected.append(selected2[0])
+        else:
+            chosen.append(selected2[0])
+            rejected.append(selected1[0])
+            
+    return chosen, rejected
+
+
+def tournament_ranking(responses: List[str], rewards: List[float]):
+    """Conducts tournament ranking. Starts from n responses and construct n-1 pairs to be assigned
+    to chosen or rejected based on there rewards.
+    
+    Args:
+        responses: accecpted candidates from rejection sampling.
+        rewards: response rewards.
+        
+    Returns:
+        chosen: chosen samples.
+        rejected: rejected samples.
+    """
+    sorted_responses = [response for _, response in sorted(zip(rewards, responses), reverse=True)]
+    
+    chosen = [sorted_responses[i] for i in range(0, len(responses), 2)]
+    rejected =[sorted_responses[i] for i in range(1, len(responses), 2)]
+    
+    return chosen, rejected
+
 
 parser = HfArgumentParser(ScriptArguments)
 script_args = parser.parse_args_into_dataclasses()[0]
@@ -90,13 +161,29 @@ print("After filtering incomplete and short responses", len(ds))
 data = []
 cnt = 0
 for sample in ds:
-    if sample['rewards'][0] < -999 or sample['rewards'][1] < -999:
+    if np.min(sample['rewards']) < -999:
         cnt += 1
         continue
-    if sample['rewards'][0] > sample['rewards'][1]:
-        data.append({"positive": sample['input'] + sample['output'][0], "negative": sample['input'] + sample['output'][1]})
-    else:
-        data.append({"positive": sample['input'] + sample['output'][1], "negative": sample['input'] + sample['output'][0]})
+    accpted, rewards = conduct_rejection_sampling(sample["output"],
+                                sample["rewards"], 
+                                script_args.num_samples_per_prompt, 
+                                script_args.beta)
+    if script_args.ranking_type == "first_round":
+        ranking_fn = first_round_ranking
+    elif script_args.ranking_type == "tournament":
+        ranking_fn = tournament_ranking
+    chosen, rejected = ranking_fn(accpted, rewards)
+
+    assert len(chosen) == len(rejected)
+
+    for i in range(len(chosen)):
+        data.append({"positive": sample['input'] + script_args.input_output_delimiter + chosen[i],
+                      "negative": sample['input'] + script_args.input_output_delimiter + rejected[i]})
+
+    #if sample['rewards'][0] > sample['rewards'][1]:
+    #    data.append({"positive": sample['input'] + sample['output'][0], "negative": sample['input'] + sample['output'][1]})
+    #else:
+    #    data.append({"positive": sample['input'] + sample['output'][1], "negative": sample['input'] + sample['output'][0]})
 
 print("Some samples are too long so deleted", cnt)
 print("We collect ", len(data), " comparison pairs")
